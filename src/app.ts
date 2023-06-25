e60b0ec581557731c096810a28a53842d092461c
NarawitC <narawit.developer@outlook.com>
2023-06-25 12:31:34 +0700

    feat(controller, service): youtubeDownload

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,20 @@
-import Fastify, { FastifyInstance, RouteHandlerMethod } from 'fastify'
+import Fastify, { FastifyInstance } from 'fastify'
 import cors from '@fastify/cors'
+import routes from './routes/index'
 const fastify: FastifyInstance = Fastify({
   logger: true,
 })
 const PORT: string = process.env.PORT || '8000'
 
-// const rootHandler: RouteHandlerMethod = async (request, reply) => {
-//   reply.send({ message: 'Hello, Fastify with TypeScript!' })
-// }
-
-// fastify.get('/', rootHandler)
 fastify.register(cors, {
-  // origin: (origin, cb) => {
-  //   const hostname = new URL(origin)
-  //   if (hostname === 'localhost') {
-  //     //  Request from localhost will pass
-  //     cb(null, true)
-  //     return
-  //   }
-  //   // Generate an error on other origins, disabling access
-  //   cb(new Error('Not allowed'), false)
-  // },\
-  origin: true,
+  origin: ['http://localhost:3000'],
 })
-fastify.register(import('./routes/index'), { prefix: '/api/v1.0' })
+fastify.register(routes, { prefix: '/api/v1.0' })
 
 const start = async () => {
   try {
     await fastify.listen({ port: parseInt(PORT) })
+    console.log('\n', '------------------')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
